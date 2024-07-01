5b35d4ddbadcc2eed9722962ef957b80d00bff4f
brunafalheiro <brunafalheiro@yahoo.com.br>
2024-07-01 18:26:39 -0300

    Verify if client exists before adding

--- src/clients/add-client.js
+++ src/clients/add-client.js
@@ -1,3 +1,17 @@
+const request = async (url) => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      return response.text();
+    } else {
+      return Promise.reject(response);
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
 const postRequest = async (url, data) => {
   try {
     const response = await fetch(url, {
@@ -22,6 +36,26 @@ async function addClient() {
   const name = document.getElementById('name').value;
   const phone = document.getElementById('phone').value;
   const address = document.getElementById('address').value;
+
+  const checkUserRegisteredQuery = `
+    PREFIX rdf: <http://www.w3.com/1999/02/22-rdf-syntax-ns#>
+    PREFIX ex: <http://example.com/>
+
+    SELECT ?cliente
+    WHERE {
+      ?cliente rdf:type ex:Cliente ;
+              ex:telefone ?telefone .
+      FILTER (?telefone = "${phone}")
+    }`;
+
+  const checkUrl = `http://localhost:7200/repositories/semantic-search?query=${encodeURIComponent(checkUserRegisteredQuery)}`;
+  const checkResponse = await request(checkUrl);
+  const checkData = checkResponse.split('\n').length == 3; // 3 because the headers line
+  if (checkData) {
+    alert('Número de telefone já cadastrado.');
+    return;
+  }
+
   const query = `
     PREFIX rdf: <http://www.w3.com/1999/02/22-rdf-syntax-ns#>
     PREFIX ex: <http://example.com/>
